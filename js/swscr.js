ed015fbcccae84544c15ea4492fa8639d2e27944
songlywings <105519171+songlywings@users.noreply.github.com>
2022-06-02 17:29:35 +0300

    Add files via upload

---
+++ js/swscr.js
@@ -0,0 +1,53 @@
+//Скрипт на кнопку "вверх"
+function upbutton() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $('#totopbtn').fadeIn();
+        } else {
+            $('#totopbtn').fadeOut();
+        }
+    });
+    $('#totopbtn').click(function() {
+        $('body,html').animate({scrollTop:0},800);
+    });
+};
+//Скрипт на прелоадер
+function preloader() {
+    const body = $('body');
+
+    body.addClass('loaded_hiding');
+    window.setTimeout(function () {
+        body.addClass('loaded');
+        body.removeClass('loaded_hiding');
+    }, 500);
+};
+//$(window).on('load', function () {
+    //preloader();
+//});
+//Скрипт на мобильное меню
+function menum() {
+    $('.openmenu').on("click", function(){ 
+        $('#menum').toggle();
+    });
+};
+//Скрипт на геолокацию
+function geolocation() {
+    if (ymaps.geolocation.country == 'Россия') {
+        jQuery(".reg_ru").show();
+        jQuery(".reg_en").hide();
+    }
+    else {
+        jQuery(".reg_ru").hide();
+        jQuery(".reg_en").show();
+    }
+};
+
+//Выполнение всех скриптов после загрузки страницы
+setTimeout(function () {
+    upbutton();
+    menum();
+    geolocation();
+}, 500);
+$(window).on('load', setTimeout(function () {
+    preloader();
+}, 3000));
